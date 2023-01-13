87efeedb189200966d4f656eee4724debb97d380
Dulce Fonseca <dulcefonseca@Dulces-MacBook-Pro.local>
2023-01-13 12:15:07 -0700

    Video2, 11:08, npm install @material-ui/icons --force

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,20 +1,38 @@
+//============== React import ==============//
 import React from "react";
+//============== Material UI import ==============//
+import Grid from "@mui/material/Grid";
+import CircularProgress from "@mui/material/CircularProgress";
+
+//============== react-redux ==============//
 import { useSelector } from "react-redux";
+
+//============== Files and Folders imports ==============//
 import Post from "./Post/Post";
 import useStyles from "./styles";
 
+//============== Code ==============//
 const Posts = () => {
   const posts = useSelector((state) => state.posts);
   const classes = useStyles();
 
   console.log(posts);
 
-  return (
-    <>
-      <h1>Posts</h1>;
-      <Post />
-      <Post />
-    </>
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid key={post._id} item xs={12} sm={6}>
+          <Post post={post} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
