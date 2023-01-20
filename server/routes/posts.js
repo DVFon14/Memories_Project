c95de9057986348ecc5e380996f5b2dc7ed359d8
Dulce Fonseca <dulcefonseca@Dulces-MacBook-Pro.local>
2023-01-20 13:58:52 -0700

    Video2, 1hr, 10min, 53sec; dotenv

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { getPosts, createPost } from "../controllers/posts.js"; //.js not required in React but it is required in node
+import {
+  getPosts,
+  createPost,
+  updatePost,
+  deletePost,
+  likePost,
+} from "../controllers/posts.js"; //.js not required in React but it is required in node
 
 const router = express.Router();
 
@@ -7,5 +13,8 @@ const router = express.Router();
 
 router.get("/", getPosts);
 router.post("/", createPost);
+router.patch("/:id", updatePost); //used for updating existing documents
+router.delete("/:id", deletePost);
+router.patch("/:id/likePost", likePost); //updating number of likes
 
 export default router;
