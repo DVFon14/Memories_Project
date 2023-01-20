d5816af86ea81f5d1814c3e934fc97f0e060b5f3
Dulce Fonseca <dulcefonseca@Dulces-MacBook-Pro.local>
2023-01-20 14:33:01 -0700

    change in api -> index

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const url = "http://localhost:3000/posts";
+// const url = "http://localhost:3000/posts";
+const url = "https://memoriesproject.onrender.com/posts";
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
