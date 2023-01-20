95476dce6445474b50e644711cdd3bf331563620
Dulce Fonseca <dulcefonseca@Dulces-MacBook-Pro.local>
2023-01-20 12:34:56 -0700

    Video1, 1hr&6min&40sec, action constants implemented

---
+++ client/src/constants/actionTypes.js
@@ -0,0 +1,7 @@
+export const CREATE = "CREATE";
+export const UPDATE = "UPDATE";
+export const DELETE = "DELETE";
+export const FETCH_ALL = "FETCH_ALL";
+
+//Now, if we misspell something in client > src > actions > post.js, catching the error
+//will be significantly easier than if we had kept them as strings
